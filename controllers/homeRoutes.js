b717e50c5295c554caa2c034288cafffc2db57fa
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-08-07 20:55:08 -0600

    created apiRoutes and added connection to handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,14 +3,16 @@ const User = require('../models/User');
 
 router.get('/', async (req, res) => {
     try {
-        const users = await User.findAll({});
-        res.json(users);
+        const usersData = await User.findAll({});
+        const users = usersData.map(user => user.toJSON());
+        // res.json(users);
+        res.render('homepage', {
+            users
+        });
     } catch (e) {
         res.status(400).json(e);
     }
-
-    // Send the rendered Handlebars.js template back as the response
-    // res.render('homepage');
+ 
 });
 
 module.exports = router;
