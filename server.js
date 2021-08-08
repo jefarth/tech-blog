020cb58fb2c2a92e0169f8e5845744bc930b55fb
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-08-07 20:53:13 -0600

    added handlebars

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
