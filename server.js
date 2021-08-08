21ea1bd910a42bd19bbb322e70888f8fb650583b
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-08-07 18:05:56 -0600

    added sequelize to server

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
+const sequelize = require('./config/connection');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(PORT, () => console.log(`Now listening at port ${PORT}`));
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening at port ${PORT}`));
+});
