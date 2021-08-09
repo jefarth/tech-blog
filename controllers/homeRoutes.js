71be67b49c94039bcb99f8535974b7b15fcf009b
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2021-08-08 23:12:35 -0600

    updated homeRoutes to show all current users

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,18 +1,30 @@
 const router = require('express').Router();
-const User = require('../models/User');
+const { User } = require('../models');
 
+// GET all users for homepage
 router.get('/', async (req, res) => {
-    try {
-        const usersData = await User.findAll({});
-        const users = usersData.map(user => user.toJSON());
-        // res.json(users);
-        res.render('homepage', {
-            users
-        });
-    } catch (e) {
-        res.status(400).json(e);
+  try {
+    const dbUserData = await User.findAll({});
+
+    const users = dbUserData.map(user => user.get({ plain: true }));
+
+    res.render('homepage', {
+      users,
+      loggedIn: req.session.loggedIn,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+      res.redirect('/');
+      return;
     }
- 
+  
+    res.render('login');
 });
 
 module.exports = router;
